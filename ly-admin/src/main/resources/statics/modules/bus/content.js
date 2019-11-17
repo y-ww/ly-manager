9c6d58d5e0e5e4dba2e37c76136bef2c1ddf490d
yww2640 <13129925589@163.com>
2019-11-17 21:26:22 +0800

    yww update imagejs

--- ly-admin/src/main/resources/statics/modules/bus/content.js
+++ ly-admin/src/main/resources/statics/modules/bus/content.js
@@ -78,6 +78,16 @@ layui.define(['table','form','upload','laytpl'],function (exports) {
     //建议使用工厂方法getEditor创建和引用编辑器实例，如果在某个闭包下引用该编辑器，直接调用UE.getEditor('editor')就能拿到相关的实例
     var ue = UE.getEditor('editor');
 
+    UE.Editor.prototype._bkGetActionUrl = UE.Editor.prototype.getActionUrl;
+    UE.Editor.prototype.getActionUrl = function(action) {
+        if (action == 'uploadimage' || action == 'uploadscrawl' || action == 'uploadimage') {
+            return 'http://localhost:8866/imgUpload';
+            //'http://localhost:8080/imgUpload';为方法imgUpload的访问地址
+        } else {
+            return this._bkGetActionUrl.call(this, action);
+        }
+    }
+
 
 
     $("#getAllHtml").click(function(){
