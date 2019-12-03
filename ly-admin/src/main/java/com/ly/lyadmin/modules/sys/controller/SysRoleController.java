b92a57386998ce5f36930b9ebff10916f01e9033
SLIGHTLEE <lmm_work@163.com>
2019-12-03 17:30:36 +0800

    add role

--- ly-admin/src/main/java/com/ly/lyadmin/modules/sys/controller/SysRoleController.java
+++ ly-admin/src/main/java/com/ly/lyadmin/modules/sys/controller/SysRoleController.java
@@ -1,8 +1,10 @@
 package com.ly.lyadmin.modules.sys.controller;
 
+import com.ly.common.utils.Result;
 import com.ly.lyadmin.modules.sys.service.SysRoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -18,5 +20,19 @@ public class SysRoleController {
     @Autowired
     SysRoleService sysRoleService;
 
-
+    /**
+     * @Description: 角色列表
+     * @Param:
+     * @Return:
+     * @Author: SLIGHTLEE
+     * @Email: lmm_work@163.com
+     * @Date: 2019/12/3 4:46 下午
+     */
+    @RequestMapping("/rolelist")
+    public Result list(@RequestParam Integer page, @RequestParam Integer limit,
+                       @RequestParam(required = false) String searchKey,
+                       @RequestParam(required = false) String searchValue){
+        Result r = sysRoleService.getRoleList(page, limit, searchKey, searchValue);
+        return r;
+    }
 }
--- ly-admin/src/main/java/com/ly/lyadmin/modules/sys/service/SysRoleService.java
+++ ly-admin/src/main/java/com/ly/lyadmin/modules/sys/service/SysRoleService.java
@@ -2,7 +2,14 @@ package com.ly.lyadmin.modules.sys.service;
 
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.ly.common.utils.Result;
 import com.ly.lyadmin.modules.sys.model.SysRole;
 
 public interface SysRoleService extends IService<SysRole> {
+
+    /**
+     *  根据条件 角色名称 模糊查询
+     */
+    Result getRoleList(Integer pageNo, Integer pageSize, String searchKey, String searchValue);
+
 }
