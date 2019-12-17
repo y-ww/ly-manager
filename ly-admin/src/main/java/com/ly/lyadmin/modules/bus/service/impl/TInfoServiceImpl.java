638260bb415909cdfe4be542da3184fab151bd5a
SLIGHTLEE <lmm_work@163.com>
2019-12-17 17:50:05 +0800

    bug update

--- ly-admin/src/main/java/com/ly/lyadmin/modules/bus/service/impl/TInfoServiceImpl.java
+++ ly-admin/src/main/java/com/ly/lyadmin/modules/bus/service/impl/TInfoServiceImpl.java
@@ -10,7 +10,9 @@ import com.ly.lyadmin.modules.bus.mapper.TInfoMapper;
 import com.ly.lyadmin.modules.bus.model.TInfo;
 import com.ly.lyadmin.modules.bus.service.TInfoService;
 import com.ly.lyadmin.modules.bus.utils.Constant;
+import com.ly.lyadmin.modules.sys.mapper.SysUserRoleMapper;
 import com.ly.lyadmin.modules.sys.model.SysUser;
+import com.ly.lyadmin.modules.sys.model.SysUserRole;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -28,17 +30,22 @@ public class TInfoServiceImpl extends ServiceImpl<TInfoMapper, TInfo> implements
     @Autowired
     TInfoMapper tInfoMapper;
 
+    @Autowired
+    SysUserRoleMapper sysUserRoleMapper;
+
     @Override
     public Result contentlist(Long userId, Integer pageNo, Integer pageSize, String searchKey, String searchValue) {
 
 
-        //  根据用户编号 查询出  平台 ID
-        String ptCode = "0";
+        //  根据用户编号 查询出 角色id 对应  平台 ID
+        QueryWrapper<SysUserRole> wrapper = new QueryWrapper<>();
+        wrapper.eq("user_id",userId);
+        SysUserRole sysUserRole = sysUserRoleMapper.selectOne(wrapper);
 
         QueryWrapper<TInfo> queryWrapper = new QueryWrapper<TInfo>();
 
-        queryWrapper.eq("platform",ptCode);
-        queryWrapper.eq("personid",userId);
+        queryWrapper.eq("platform",sysUserRole.getRoleId());
+     //   queryWrapper.eq("personid",userId);
         queryWrapper.eq("isdelete",1);
         queryWrapper.eq("is_fbtype",1);
         queryWrapper.orderByDesc("create_time");
